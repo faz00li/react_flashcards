72afed2bf5c351be5ffe7ba1957b240675f31626
Eddie Raskin <eddie.raskin@gmail.com>
2020-06-25 15:29:52 -0700

    add components, style, react_db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Card from './Card/Card';
+import DrawButton from './DrawButton/DrawButton';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { DB_CONFIG } from './Config/Firebase/db_config';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('cards');
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [],
+      currentCard: {} 
+    }
+  }
+
+  componentWillMount(){
+    console.log(this.app.database().ref().child('cards'))
+    const currentCards = this.state.cards;
+    this.database.on('child_added', snap => {
+      currentCards.push({
+        id: snap.key,
+        eng: snap.val().eng,
+        han: snap.val().han,
+        pin: snap.val().pin,
+      })
+
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      })
+
+    })
+  }
+
+  getRandomCard(currentCards){
+    var randomIndex = Math.floor(Math.random() * currentCards.length);
+    var card = currentCards[randomIndex];
+    if(card === this.state.currentCard){
+      this.getRandomCard(currentCards)
+    }
+
+    return(card);
+  }
+
+  updateCard(){
+    const currentCards = this.state.cards;
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="cardRow">
+          <Card eng={this.state.currentCard.eng}
+            han={this.state.currentCard.han}
+            pin={this.state.currentCard.pin}
+          />
+        </div>
+        <div className="buttonRow">
+          <DrawButton drawCard={this.updateCard}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
